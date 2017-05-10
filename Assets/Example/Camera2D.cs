32f06c3fa952079067ab78d888758e9beb0095a9
Alexander Verbeek <sanderman@gmail.com>
2017-05-10 10:52:54 +0200

    Added Camera2D helper script to set camera size in pixels per unit/meter.

---
+++ Assets/Example/Camera2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A 2D orthographic camera helper.
+/// </summary>
+public class Camera2D : MonoBehaviour {
+
+    [SerializeField]
+    private int pixelsPerMeter = 32; // Eg. setting this to 32 means that an area of 1 meter squared will occupy 32x32 of screen real estate.
+
+    private Camera cam;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+	void Update () {
+        cam.orthographicSize = Screen.height / pixelsPerMeter;
+	}
+}
