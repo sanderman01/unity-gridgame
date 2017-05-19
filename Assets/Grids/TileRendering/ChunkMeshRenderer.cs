eaf217930a5bfc44050f813db556254930dd1bbf
Alexander Verbeek <sanderman@gmail.com>
2017-05-19 16:30:37 +0200

    Add ChunkMeshRenderer, in preparation for splitting tile rendering out over multiple meshes, where each chunk will have its own mesh.

---
+++ Assets/Grids/TileRendering/ChunkMeshRenderer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace AmarokGames.Grids {
+
+    /// <summary>
+    /// When added to 2D grid chunks, this component handles some minimal bookkeeping and rendering for meshes created by tile rendering system.
+    /// Using this, a tile rendering system can easily attach a mesh to a chunk without bothering about the details of creating a child object and setting up components.
+    /// 
+    /// Note: Thruthfully, I would rather prefer to keep all meshes stored in a centralised system and render them using Graphics.DrawMesh,
+    /// but there are some advantages to doing mesh rendering the more conventional way, using MeshFilter and MeshRenderer components.
+    /// One of these is the ability to pauze the game within the unity editor and move the camera to inspect chunks. 
+    /// That will not work properly if using Graphics.DrawMesh in an Update.
+    /// </summary>
+    public class ChunkMeshRenderer : MonoBehaviour {
+
+        public Mesh Mesh {
+            get { return meshFilter.sharedMesh; }
+            set { meshFilter.sharedMesh = value; }
+        }
+
+        public Material Material {
+            get { return meshRenderer.sharedMaterial; }
+            set { meshRenderer.sharedMaterial = value; }
+        }
+
+        public MeshFilter MeshFilter { get { return meshFilter; } }
+        public MeshRenderer MeshRenderer { get { return meshRenderer; } }
+
+        private MeshFilter meshFilter;
+        private MeshRenderer meshRenderer;
+
+        public static ChunkMeshRenderer Create(string name, GameObject parentChunk, Material material, Mesh mesh) {
+
+            Assert.IsNotNull(parentChunk, "parentChunk is null");
+            Assert.IsNotNull(material, "material is null");
+            Assert.IsNotNull(mesh, "mesh is null");
+
+            // Create object and components
+            GameObject obj = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer), typeof(ChunkMeshRenderer));
+            obj.transform.SetParent(parentChunk.transform, false);
+            ChunkMeshRenderer result = obj.GetComponent<ChunkMeshRenderer>();
+            result.meshFilter = result.GetComponent<MeshFilter>();
+            result.meshRenderer = result.GetComponent<MeshRenderer>();
+
+            // Set values;
+            result.meshFilter.sharedMesh = mesh;
+            result.meshRenderer.sharedMaterial = material;
+
+            return result;
+        }
+    }
+}
+
