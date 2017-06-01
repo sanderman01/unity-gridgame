ab879b2441153a3e6e97aa6d2345137496070b18
Alexander Verbeek <sanderman@gmail.com>
2017-06-01 11:58:44 +0200

    Extract ChunkKey to its own file and make it public. Turns out its useful for various tile rendering and similar systems.

--- Assets/Grids/TileRendering2D/GridTileRenderer.cs
+++ Assets/Grids/TileRendering2D/GridTileRenderer.cs
@@ -435,36 +435,5 @@ namespace AmarokGames.Grids {
 
             vertexCount += 4;
         }
-
-
-        struct ChunkKey {
-            public int gridId;
-            public Int2 chunkCoord;
-
-            public ChunkKey(int gridId, Int2 chunkCoord) {
-                this.gridId = gridId;
-                this.chunkCoord = chunkCoord;
-            }
-
-            public bool Equals(ChunkKey other) {
-                return this.gridId == other.gridId && this.chunkCoord == other.chunkCoord;
-            }
-
-            public override bool Equals(object obj) {
-                if (obj is ChunkKey) {
-                    ChunkKey other = (ChunkKey)obj;
-                    return Equals(other);
-                } else {
-                    return false;
-                }
-            }
-
-            public override int GetHashCode() {
-                int hash = 17;
-                hash *= hash * 23 + gridId.GetHashCode();
-                hash *= hash * 23 + chunkCoord.GetHashCode();
-                return hash;
-            }
-        }
     }
 }
