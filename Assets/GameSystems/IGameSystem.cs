fe5ac3ee367d09354d22250b98905b1cee987dad
Alexander Verbeek <sanderman@gmail.com>
2017-06-03 19:39:29 +0200

    Define IGameSystem interface

---
+++ Assets/GameSystems/IGameSystem.cs
@@ -0,0 +1,10 @@
+﻿using AmarokGames.GridGame;
+using AmarokGames.Grids;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameSystem {
+
+    void Update(World world, IEnumerable<Grid2D> grids);
+}
