769dbb327eeef6719b11082cdf0c35d538001165
Alexander Verbeek <sanderman@gmail.com>
2017-06-04 20:33:35 +0200

    Add GameSystemBase optional abstract base class for IGameSystems.

---
+++ Assets/GameSystems/GameSystemBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace AmarokGames.GridGame {
+
+    public abstract class GameSystemBase : MonoBehaviour, IGameSystem {
+
+        public static T Create<T>(string name) where T : MonoBehaviour {
+            GameObject gameObject = new GameObject(name);
+            T sys = gameObject.AddComponent<T>();
+            return sys;
+        }
+
+        protected abstract void Disable();
+        protected abstract void Enable();
+
+        public bool Enabled { get { return this.enabled; } set { this.enabled = value; } }
+
+        public void OnEnable() { this.Enable(); }
+        public void OnDisable() { this.Disable(); }
+
+        public abstract void TickWorld(World world, int tickRate);
+        public abstract void UpdateWorld(World world, float deltaTime);
+    }
+}
