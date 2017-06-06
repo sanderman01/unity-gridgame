2c1b53b9e3213fb2fc91fb363b643b414caf4ef3
Alexander Verbeek <sanderman@gmail.com>
2017-06-06 14:03:09 +0200

    Move a lot of setup stuff from Main to BaseGameMod.

--- Assets/Game/Main.cs
+++ Assets/Game/Main.cs
@@ -13,10 +13,6 @@ namespace AmarokGames.GridGame {
         [SerializeField]
         private Int2 worldChunkSize = new Int2(64, 64);
 
-        // TODO REMOVE THIS WHEN NO LONGER NEEDED FOR DEBUGGING
-        [SerializeField]
-        private Texture2D tempTileAtlasTex;
-
         private TileRegistry tileRegistry;
         private World world;
 
@@ -31,48 +27,24 @@ namespace AmarokGames.GridGame {
         LayerId tileBackgroundLayerUShort;
         LayerId terrainGenDebugLayerFloat;
 
+        BaseGameMod baseGameMod;
+
         public void Start() {
+
+            layers = new LayerConfig();
             tileRegistry = new TileRegistry();
 
-            layers = new LayerConfig()
-                .AddLayer("solid", BufferType.Boolean, out solidLayerBool)
-                .AddLayer("tileforeground", BufferType.UShort, out tileForegroundLayerUShort)
-                .AddLayer("tilebackground", BufferType.UShort, out tileBackgroundLayerUShort)
-                .AddLayer("terrainGenDebugLayer", BufferType.Float, out terrainGenDebugLayerFloat);
+            baseGameMod = new BaseGameMod();
+            baseGameMod.Init(ref layers, tileRegistry, gameSystems);
 
-            RegisterTiles(tileRegistry);
-            CreateWorld(0);
-            CreateRenderers();
-            gameSystems.Add(GridCollisionSystem.Create(new Grids.Data.LayerId(0)));
-        }
-
-        private void RegisterTiles(TileRegistry tileRegistry) {
-
-            Tile empty = new Tile();
-            empty.CollisionSolid = false;
-            empty.BatchedRendering = false;
-            empty.HumanName = "Empty";
-            Texture2D emptyTex = Resources.Load<Texture2D>("tile-empty");
-
-            Tile stone = new Tile();
-            stone.CollisionSolid = true;
-            stone.BatchedRendering = true;
-            stone.HumanName = "Stone";
-            Texture2D stoneTex = Resources.Load<Texture2D>("tile-stone");
-
-            Tile concrete = new Tile();
-            concrete.CollisionSolid = true;
-            concrete.BatchedRendering = true;
-            concrete.HumanName = "Concrete";
-            Texture2D concreteTex = Resources.Load<Texture2D>("tile-concrete");
-
-            tileRegistry.RegisterTile("vanilla", "empty", empty, emptyTex);
-            tileRegistry.RegisterTile("vanilla", "stone", stone, stoneTex);
-            tileRegistry.RegisterTile("vanilla", "concrete", concrete, concreteTex);
+            solidLayerBool = baseGameMod.solidLayerBool;
+            tileForegroundLayerUShort = baseGameMod.tileForegroundLayerUShort;
+            tileBackgroundLayerUShort = baseGameMod.tileBackgroundLayerUShort;
+            terrainGenDebugLayerFloat = baseGameMod.terrainGenDebugLayerFloat;
 
             tileRegistry.Finalise();
 
-            tempTileAtlasTex = tileRegistry.GetAtlas().GetTexture();
+            baseGameMod.PostInit(tileRegistry, gameSystems);
 
             // Add Player
             player = new Player();
@@ -82,68 +54,14 @@ namespace AmarokGames.GridGame {
             playerCharacter.Possess(player);
 
             Camera.main.GetComponent<Camera2D>().Target = playerCharacter.transform;
+
+            CreateWorld(0);
         }
 
         private void CreateWorld(int seed) {
-            WorldGenerator worldGen = new WorldGenerator(solidLayerBool, tileForegroundLayerUShort, tileBackgroundLayerUShort, terrainGenDebugLayerFloat);
+            WorldGenerator worldGen = baseGameMod.GetWorldGenerator();
             world = World.CreateWorld("world", 0, worldSize, worldChunkSize, layers, worldGen);
             world.WorldGenerator.Init(world);
-        }
-
-        private void CreateRenderers() {
-
-            // TODO Clean up this mess. Where should we put this renderer setup stuff?
-            {
-                // Solid Renderer
-                Shader shader = Shader.Find("Sprites/Default");
-                Material mat = new Material(shader);
-                mat.color = new Color(1, 1, 1, 0.5f);
-                GridSolidRendererSystem solidRenderer = GridSolidRendererSystem.Create(mat, solidLayerBool);
-                gameSystems.Add(solidRenderer);
-                solidRenderer.Enabled = false;
-            }
-
-            {
-                Shader shader = Shader.Find("Sprites/Default");
-                Material mat = new Material(shader);
-                mat.color = new Color(1, 1, 1, 0.5f);
-                BufferVisualiserFloat sys = BufferVisualiserFloat.Create(mat, terrainGenDebugLayerFloat);
-                gameSystems.Add(sys);
-                sys.Enabled = false;
-            }
-
-            {
-                // Tile Renderer
-                Texture2D textureAtlas = tileRegistry.GetAtlas().GetTexture();
-
-                Shader foregroundShader = Shader.Find("Unlit/Transparent Cutout");
-                Material foregroundMaterial = new Material(foregroundShader);
-                foregroundMaterial.mainTexture = textureAtlas;
-                foregroundMaterial.color = new Color(1, 1, 1, 1);
-
-                Shader backgroundShader = Shader.Find("Sprites/Default");
-                Material backgroundMaterial = new Material(backgroundShader);
-                backgroundMaterial.mainTexture = textureAtlas;
-                backgroundMaterial.color = new Color(0.5f, 0.5f, 0.5f, 1);
-
-                IList<Tile> tiles = tileRegistry.GetTiles();
-                int tileCount = tiles.Count;
-                TileRenderData[] tileData = new TileRenderData[tileCount];
-                for(int i = 0; i < tileCount; ++i) {
-                    Tile tile = tiles[i];
-                    TileRenderData d = new TileRenderData();
-                    d.draw = tile.BatchedRendering;
-                    d.zLayer = (ushort)i;
-                    d.variants = new TileVariant[1];
-                    d.variants[0] = new TileVariant(
-                        new Vector2(tile.SpriteUV.x, tile.SpriteUV.y),
-                        new Vector2(tile.SpriteUV.xMax, tile.SpriteUV.yMax));
-                    tileData[i] = d;
-                }
-
-                gameSystems.Add(GridTileRenderSystem.Create(tileData, foregroundMaterial, tileForegroundLayerUShort, 0));
-                gameSystems.Add(GridTileRenderSystem.Create(tileData, backgroundMaterial, tileBackgroundLayerUShort, 1));
-            }
         }
 
         void Update() {
