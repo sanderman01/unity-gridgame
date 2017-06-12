7a1a0872f7afe4229c29e412accef57420115944
Alexander Verbeek <sanderman@gmail.com>
2017-06-12 13:31:46 +0200

    Put Player and PlayerCharacter in namespace.

--- Assets/Game/Player.cs
+++ Assets/Game/Player.cs
@@ -2,39 +2,42 @@
 
 using UnityEngine;
 
-public class Player {
+namespace AmarokGames.GridGame {
 
-    private PlayerCharacter playerCharacter;
-    public PlayerCharacter Character { get { return playerCharacter; } }
+    public class Player {
 
-    private const KeyCode KeyLeft = KeyCode.A;
-    private const KeyCode KeyRight = KeyCode.D;
-    private const KeyCode KeyUp = KeyCode.W;
-    private const KeyCode KeyDown = KeyCode.S;
-    private const KeyCode KeyJump = KeyCode.Space;
+        private PlayerCharacter playerCharacter;
+        public PlayerCharacter Character { get { return playerCharacter; } }
 
-    public bool Left { get; set; }
-    public bool Right { get; set; }
-    public bool Down { get; set; }
-    public bool Up { get; set; }
-    public bool Jump { get; set; }
+        private const KeyCode KeyLeft = KeyCode.A;
+        private const KeyCode KeyRight = KeyCode.D;
+        private const KeyCode KeyUp = KeyCode.W;
+        private const KeyCode KeyDown = KeyCode.S;
+        private const KeyCode KeyJump = KeyCode.Space;
 
-	public void Update () {
-        Left = Input.GetKey(KeyLeft);
-        Right = Input.GetKey(KeyRight);
-        Up = Input.GetKey(KeyUp);
-        Down = Input.GetKey(KeyDown);
-        Jump = Input.GetKey(KeyJump);
-    }
+        public bool Left { get; set; }
+        public bool Right { get; set; }
+        public bool Down { get; set; }
+        public bool Up { get; set; }
+        public bool Jump { get; set; }
 
-    public void Possess(PlayerCharacter playerCharacter) {
-        if (this.playerCharacter != null) {
-            this.playerCharacter.player = null;
+        public void Update() {
+            Left = Input.GetKey(KeyLeft);
+            Right = Input.GetKey(KeyRight);
+            Up = Input.GetKey(KeyUp);
+            Down = Input.GetKey(KeyDown);
+            Jump = Input.GetKey(KeyJump);
         }
 
-        if (playerCharacter != null) {
-            playerCharacter.player = this;
+        public void Possess(PlayerCharacter playerCharacter) {
+            if (this.playerCharacter != null) {
+                this.playerCharacter.player = null;
+            }
+
+            if (playerCharacter != null) {
+                playerCharacter.player = this;
+            }
+            this.playerCharacter = playerCharacter;
         }
-        this.playerCharacter = playerCharacter;
     }
 }
--- Assets/Game/PlayerCharacter.cs
+++ Assets/Game/PlayerCharacter.cs
@@ -1,179 +1,181 @@
 ﻿// Copyright(C) 2017 Amarok Games, Alexander Verbeek
 
-using System;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+namespace AmarokGames.GridGame {
 
-    internal Player player;
-    private new Rigidbody2D rigidbody;
+    public class PlayerCharacter : MonoBehaviour {
 
-    [SerializeField]
-    private bool grounded;
-    [SerializeField]
-    private bool allowJetpack = true;
+        internal Player player;
+        private new Rigidbody2D rigidbody;
 
-    [SerializeField]
-    private bool showCollisionGizmos;
+        [SerializeField]
+        private bool grounded;
+        [SerializeField]
+        private bool allowJetpack = true;
 
-    // Debugging aides
-    private ContactPoint2D[] points = new ContactPoint2D[0];
+        [SerializeField]
+        private bool showCollisionGizmos;
 
-    // Fields used for step-assist
-    private bool applyStepAssistNextFrame;
-    private float stepAssistHeight;
-    private float previousFrameHorizontalVelocity;
+        // Debugging aides
+        private ContactPoint2D[] points = new ContactPoint2D[0];
 
-    new private Collider2D collider;
+        // Fields used for step-assist
+        private bool applyStepAssistNextFrame;
+        private float stepAssistHeight;
+        private float previousFrameHorizontalVelocity;
 
-    void Awake() {
-        rigidbody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
-    }
+        new private Collider2D collider;
 
-    void FixedUpdate() {
-
-        const float maxWalkingSpeed = 15f;
-        const float walkingAcceleration = 50f;
-        const float gravityAcceleration = 50;
-        const float jetpackAcceleration = 50f;
-        const float dragAcceleration = 10f;
-        const float jumpVelocity = 10f;
-
-        if (player.Right && rigidbody.velocity.x < maxWalkingSpeed) {
-            // Move right
-            Vector2 vel = rigidbody.velocity;
-            vel.x += walkingAcceleration * Time.fixedDeltaTime;
-            rigidbody.velocity = vel;
+        void Awake() {
+            rigidbody = GetComponent<Rigidbody2D>();
+            collider = GetComponent<Collider2D>();
         }
 
-        if (player.Left && rigidbody.velocity.x > -maxWalkingSpeed) {
-            // Move left
-            Vector2 vel = rigidbody.velocity;
-            vel.x -= walkingAcceleration * Time.fixedDeltaTime;
-            rigidbody.velocity = vel;
-        }
+        void FixedUpdate() {
 
-        if(!(player.Left || player.Right)) {
-            float drag = dragAcceleration * Time.fixedDeltaTime * rigidbody.velocity.x;
-            Vector2 vel = rigidbody.velocity;
-            vel.x -= drag;
-            rigidbody.velocity = vel;
-        }
+            const float maxWalkingSpeed = 15f;
+            const float walkingAcceleration = 50f;
+            const float gravityAcceleration = 50;
+            const float jetpackAcceleration = 50f;
+            const float dragAcceleration = 10f;
+            const float jumpVelocity = 10f;
 
-        if (player.Jump && grounded) {
-            Vector2 vel = rigidbody.velocity;
-            vel.y += jumpVelocity;
-            rigidbody.velocity = vel;
-            grounded = false;
-        }
-        if (allowJetpack && player.Jump && !grounded) {
-            // Jetpack
-            const float jetpackMaxVerticalSpeed = 10;
-            Vector3 vel = rigidbody.velocity;
-            if (vel.y < jetpackMaxVerticalSpeed) {
-                vel.y = Mathf.Min(rigidbody.velocity.y + jetpackAcceleration, jetpackMaxVerticalSpeed);
+            if (player.Right && rigidbody.velocity.x < maxWalkingSpeed) {
+                // Move right
+                Vector2 vel = rigidbody.velocity;
+                vel.x += walkingAcceleration * Time.fixedDeltaTime;
                 rigidbody.velocity = vel;
             }
-        } else if (!grounded) {
-            // Get pulled down by gravity
-            Vector2 vel = rigidbody.velocity;
-            vel.y -= gravityAcceleration * Time.fixedDeltaTime;
-            rigidbody.velocity = vel;
+
+            if (player.Left && rigidbody.velocity.x > -maxWalkingSpeed) {
+                // Move left
+                Vector2 vel = rigidbody.velocity;
+                vel.x -= walkingAcceleration * Time.fixedDeltaTime;
+                rigidbody.velocity = vel;
+            }
+
+            if (!(player.Left || player.Right)) {
+                float drag = dragAcceleration * Time.fixedDeltaTime * rigidbody.velocity.x;
+                Vector2 vel = rigidbody.velocity;
+                vel.x -= drag;
+                rigidbody.velocity = vel;
+            }
+
+            if (player.Jump && grounded) {
+                Vector2 vel = rigidbody.velocity;
+                vel.y += jumpVelocity;
+                rigidbody.velocity = vel;
+                grounded = false;
+            }
+            if (allowJetpack && player.Jump && !grounded) {
+                // Jetpack
+                const float jetpackMaxVerticalSpeed = 10;
+                Vector3 vel = rigidbody.velocity;
+                if (vel.y < jetpackMaxVerticalSpeed) {
+                    vel.y = Mathf.Min(rigidbody.velocity.y + jetpackAcceleration, jetpackMaxVerticalSpeed);
+                    rigidbody.velocity = vel;
+                }
+            } else if (!grounded) {
+                // Get pulled down by gravity
+                Vector2 vel = rigidbody.velocity;
+                vel.y -= gravityAcceleration * Time.fixedDeltaTime;
+                rigidbody.velocity = vel;
+            }
+
+            rigidbody.MovePosition(rigidbody.position + rigidbody.velocity * Time.fixedDeltaTime);
+
+            // Apply the step-assist by moving the character to same y-value as the highest contact point.
+            Vector2 position = transform.position;
+            if (applyStepAssistNextFrame && stepAssistHeight < position.y + 1.1f) {
+                position.y = stepAssistHeight;
+                transform.position = position;
+                Vector2 vel = rigidbody.velocity;
+                vel.x = previousFrameHorizontalVelocity;
+                rigidbody.velocity = vel;
+            }
+            applyStepAssistNextFrame = false;
+            stepAssistHeight = transform.position.y;
+
+            DoGroundedRaycasts();
         }
 
-        rigidbody.MovePosition(rigidbody.position + rigidbody.velocity * Time.fixedDeltaTime);
+        private void DoGroundedRaycasts() {
+            Bounds bounds = collider.bounds;
+            Vector2 bottomLeft = new Vector2(bounds.min.x - 0.1f, bounds.min.y - 0.01f);
+            Vector2 bottomRight = new Vector2(bounds.max.x + 0.1f, bounds.min.y - 0.01f);
+            Vector2 rayCastDir = Vector2.down;
+            float raycastLength = 0.1f;
 
-        // Apply the step-assist by moving the character to same y-value as the highest contact point.
-        Vector2 position = transform.position;
-        if (applyStepAssistNextFrame && stepAssistHeight < position.y + 1.1f) {
-            position.y = stepAssistHeight;
-            transform.position = position;
-            Vector2 vel = rigidbody.velocity;
-            vel.x = previousFrameHorizontalVelocity;
-            rigidbody.velocity = vel;
+            bool leftHit = Physics2D.Raycast(bottomLeft, rayCastDir, raycastLength);
+            bool rightHit = Physics2D.Raycast(bottomRight, rayCastDir, raycastLength);
+
+            grounded = leftHit || rightHit;
         }
-        applyStepAssistNextFrame = false;
-        stepAssistHeight = transform.position.y;
 
-        DoGroundedRaycasts();
-    }
+        void OnCollisionEnter2D(Collision2D collision) {
+            HandleCollision(collision);
+            if (showCollisionGizmos) points = collision.contacts;
+        }
 
-    private void DoGroundedRaycasts() {
-        Bounds bounds = collider.bounds;
-        Vector2 bottomLeft = new Vector2(bounds.min.x - 0.1f, bounds.min.y - 0.01f);
-        Vector2 bottomRight = new Vector2(bounds.max.x + 0.1f, bounds.min.y - 0.01f);
-        Vector2 rayCastDir = Vector2.down;
-        float raycastLength = 0.1f;
+        void OnCollisionStay2D(Collision2D collision) {
+            HandleCollision(collision);
+            if (showCollisionGizmos) points = collision.contacts;
+        }
 
-        bool leftHit = Physics2D.Raycast(bottomLeft, rayCastDir, raycastLength);
-        bool rightHit = Physics2D.Raycast(bottomRight, rayCastDir, raycastLength);
-
-        grounded = leftHit || rightHit;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision) {
-        HandleCollision(collision);
-        if(showCollisionGizmos) points = collision.contacts;
-    }
-
-    void OnCollisionStay2D(Collision2D collision) {
-        HandleCollision(collision);
-        if(showCollisionGizmos) points = collision.contacts;
-    }
-
-    void OnCollisionExit2D(Collision2D collision) {
-    }
+        void OnCollisionExit2D(Collision2D collision) {
+        }
 
 
-    void HandleCollision(Collision2D collision) {
-        Vector3 oldVelocity = rigidbody.velocity;
-        Collider2D[] overlapping = new Collider2D[1];
-        if (collision.collider.OverlapCollider(new ContactFilter2D(), overlapping) > 0) {
-            Vector3 collisionNormal = collision.contacts[0].normal;
-            float separation = collision.contacts[0].separation;
+        void HandleCollision(Collision2D collision) {
+            Vector3 oldVelocity = rigidbody.velocity;
+            Collider2D[] overlapping = new Collider2D[1];
+            if (collision.collider.OverlapCollider(new ContactFilter2D(), overlapping) > 0) {
+                Vector3 collisionNormal = collision.contacts[0].normal;
+                float separation = collision.contacts[0].separation;
 
-            // Kill rigidbody.velocity in the direction of the collision.
-            if (Vector2.Dot(collisionNormal, rigidbody.velocity) < 0)
-                rigidbody.velocity = Vector3.ProjectOnPlane(rigidbody.velocity, collisionNormal);
+                // Kill rigidbody.velocity in the direction of the collision.
+                if (Vector2.Dot(collisionNormal, rigidbody.velocity) < 0)
+                    rigidbody.velocity = Vector3.ProjectOnPlane(rigidbody.velocity, collisionNormal);
 
-            // Reduce overlap
-            if (separation < -0.1f) {
-                transform.Translate(0.9f * -separation * collisionNormal);
-                Vector3 oldPosition = transform.position;
-                if (Mathf.Abs(collision.contacts[0].point.y - oldPosition.y) < 0.1f)
-                    grounded = true;
+                // Reduce overlap
+                if (separation < -0.1f) {
+                    transform.Translate(0.9f * -separation * collisionNormal);
+                    Vector3 oldPosition = transform.position;
+                    if (Mathf.Abs(collision.contacts[0].point.y - oldPosition.y) < 0.1f)
+                        grounded = true;
+                }
+            }
+
+            // Step-assist
+            // Iterate over contact points to determine if we should do a step-assist this frame
+            foreach (ContactPoint2D p in collision.contacts) {
+                const float thresholdAngle = 45f;
+                float angleRight = Vector2.Angle(p.normal, Vector2.right);
+                float angleLeft = Vector2.Angle(p.normal, -Vector2.right);
+                bool verticalWall = Mathf.Abs(angleRight) < thresholdAngle || Mathf.Abs(angleLeft) < thresholdAngle;
+
+                if (verticalWall && grounded) {
+                    // Schedule step-assist
+                    // Do a 1-tile jump, because this way we don't need to worry about ceilings
+                    applyStepAssistNextFrame = true;
+                    stepAssistHeight = Mathf.Max(stepAssistHeight, p.point.y);
+                    previousFrameHorizontalVelocity = oldVelocity.x;
+                }
             }
         }
 
-        // Step-assist
-        // Iterate over contact points to determine if we should do a step-assist this frame
-        foreach (ContactPoint2D p in collision.contacts) {
-            const float thresholdAngle = 45f;
-            float angleRight = Vector2.Angle(p.normal, Vector2.right);
-            float angleLeft = Vector2.Angle(p.normal, -Vector2.right);
-            bool verticalWall = Mathf.Abs(angleRight) < thresholdAngle || Mathf.Abs(angleLeft) < thresholdAngle;
-
-            if(verticalWall && grounded) {
-                // Schedule step-assist
-                // Do a 1-tile jump, because this way we don't need to worry about ceilings
-                applyStepAssistNextFrame = true;
-                stepAssistHeight = Mathf.Max(stepAssistHeight, p.point.y);
-                previousFrameHorizontalVelocity = oldVelocity.x;
-            }
+        void OnDrawGizmos() {
+            if (showCollisionGizmos) DrawGizmosContactPoints(points);
         }
-    }
 
-    void OnDrawGizmos() {
-        if(showCollisionGizmos) DrawGizmosContactPoints(points);
-    }
-
-    void DrawGizmosContactPoints(ContactPoint2D[] points) {
-        foreach (ContactPoint2D p in points) {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(p.point, 0.1f);
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(p.point, p.point + p.normal);
+        void DrawGizmosContactPoints(ContactPoint2D[] points) {
+            foreach (ContactPoint2D p in points) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(p.point, 0.1f);
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(p.point, p.point + p.normal);
+            }
         }
     }
 }
