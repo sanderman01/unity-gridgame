3a7b7518e0f0705b5ee2f5b93cfa57b39a90b175
Alexander Verbeek <sanderman@gmail.com>
2017-11-22 22:37:05 +0100

    Use a quadtree approach to generating grid collider rectangles.

--- Assets/GameSystems/GridCollisionSystem.cs
+++ Assets/GameSystems/GridCollisionSystem.cs
@@ -102,69 +102,21 @@ namespace AmarokGames.Grids {
 
             Profiler.BeginSample("UpdateColliders");
             GameObject colliderGameObject = chunkCollidersEntry.chunkColliderObject;
-            if(colliderGameObject != null) {
+            if(colliderGameObject != null)
+            {
                 // Remove any existing colliders
-                foreach (BoxCollider2D collider in chunkCollidersEntry.colliders) {
+                foreach (BoxCollider2D collider in chunkCollidersEntry.colliders)
+                {
                     UnityEngine.Object.Destroy(collider);
                 }
                 chunkCollidersEntry.colliders.Clear();
 
-                // Do row scan to determine all the rectangles we want to create box colliders for
-                List<Rect> rects = new List<Rect>();
+                // Calculate desired collider rectangles.
+                List<Rect> rects = QuadTreeColliderGen.GenerateColliders(solidBuffer);
 
-                bool makingRect;
-                Rect currentRect = new Rect();
-                for(int y = 0; y < Grid2D.ChunkHeight; ++y) {
-                    makingRect = false;
-                    for(int x = 0; x < Grid2D.ChunkWidth; ++x) {
-                        Int2 localCoord = new Int2(x, y);
-                        int cellIndex = Grid2D.GetChunkCellIndex(localCoord, Grid2D.ChunkWidth);
-                        bool solid = solidBuffer.GetValue(cellIndex);
-
-                        if(solid && !makingRect) {
-                            // Start a new rect
-                            makingRect = true;
-                            currentRect.yMin = y;
-                            currentRect.yMax = y + 1;
-                            currentRect.xMin = x;
-                        } else if(!solid && makingRect) {
-                            // finish the current rect
-                            makingRect = false;
-                            currentRect.xMax = x;
-                            rects.Add(currentRect);
-                        }
-                    }
-                    
-                    if(makingRect) {
-                        // finish the current rect
-                        makingRect = false;
-                        currentRect.xMax = Grid2D.ChunkWidth;
-                        rects.Add(currentRect);
-                    }
-                }
-
-                for(int i = rects.Count - 1; i > 0; --i) {
-                    Rect a = rects[i];
-                    Rect b = rects[i - 1];
-                    if(a.xMin == b.xMin && a.xMax == b.xMax)
-                    {
-                        // Then merge them in to one larger rect
-                        Rect newRect = new Rect();
-                        newRect.xMin = a.xMin;
-                        newRect.xMax = a.xMax;
-                        newRect.yMin = b.yMin;
-                        newRect.yMax = a.yMax;
-                        // remove the old ones from the list
-                        rects.RemoveAt(i);
-                        rects.RemoveAt(i - 1);
-
-                        // put the new one in the list
-                        rects.Insert(i-1, newRect);
-                    }
-                }
-                
                 // Get the game object, and create all the required box colliders.
-                foreach(Rect rect in rects) {
+                foreach (Rect rect in rects)
+                {
                     BoxCollider2D col = colliderGameObject.AddComponent<BoxCollider2D>();
                     col.size = rect.size;
                     //col.offset = rect.position; // + 0.5f * Vector2.one;
@@ -172,12 +124,176 @@ namespace AmarokGames.Grids {
                     chunkCollidersEntry.colliders.Add(col);
                 }
 
-            } else {
+            }
+            else {
                 // The chunk did not exist for some reason
                 // Clean lingering entries in our bookkeeping.
                 chunksColliders.Remove(new ChunkKey(worldId, grid.GridId, chunkCoord));
             }
             Profiler.EndSample();
+        }
+
+
+    }
+
+    /// <summary>
+    /// This generates collider rectangles by scanning along rows in an attempt to merge collider rectangles.
+    /// </summary>
+    public class RowScanColliderGen
+    {
+        private static List<Rect> GenerateColliders(BitBuffer solidBuffer)
+        {
+            Profiler.BeginSample("GenerateColliders");
+            List<Rect> rects = new List<Rect>();
+            bool makingRect;
+            Rect currentRect = new Rect();
+            for (int y = 0; y < Grid2D.ChunkHeight; ++y)
+            {
+                makingRect = false;
+                for (int x = 0; x < Grid2D.ChunkWidth; ++x)
+                {
+                    Int2 localCoord = new Int2(x, y);
+                    int cellIndex = Grid2D.GetChunkCellIndex(localCoord, Grid2D.ChunkWidth);
+                    bool solid = solidBuffer.GetValue(cellIndex);
+
+                    if (solid && !makingRect)
+                    {
+                        // Start a new rect
+                        makingRect = true;
+                        currentRect.yMin = y;
+                        currentRect.yMax = y + 1;
+                        currentRect.xMin = x;
+                    }
+                    else if (!solid && makingRect)
+                    {
+                        // finish the current rect
+                        makingRect = false;
+                        currentRect.xMax = x;
+                        rects.Add(currentRect);
+                    }
+                }
+
+                if (makingRect)
+                {
+                    // finish the current rect
+                    makingRect = false;
+                    currentRect.xMax = Grid2D.ChunkWidth;
+                    rects.Add(currentRect);
+                }
+            }
+
+            for (int i = rects.Count - 1; i > 0; --i)
+            {
+                Rect a = rects[i];
+                Rect b = rects[i - 1];
+                if (a.xMin == b.xMin && a.xMax == b.xMax)
+                {
+                    // Then merge them in to one larger rect
+                    Rect newRect = new Rect();
+                    newRect.xMin = a.xMin;
+                    newRect.xMax = a.xMax;
+                    newRect.yMin = b.yMin;
+                    newRect.yMax = a.yMax;
+                    // remove the old ones from the list
+                    rects.RemoveAt(i);
+                    rects.RemoveAt(i - 1);
+
+                    // put the new one in the list
+                    rects.Insert(i - 1, newRect);
+                }
+            }
+            Profiler.EndSample();
+            return rects;
+        }
+    }
+
+    /// <summary>
+    /// Calculates collider rectangles from a tile BitBuffer, using a Region-QuadTree approach.
+    /// </summary>
+    public class QuadTreeColliderGen
+    {
+        public class TreeNode
+        {
+            public enum NodeValue { Empty, Solid, Partial }
+
+            public NodeValue value;
+            public int minX;
+            public int minY;
+            public int width;
+            public TreeNode[] children;
+
+            public TreeNode(NodeValue value, int minX, int minY, int width, TreeNode[] children)
+            {
+                this.value = value;
+                this.minX = minX;
+                this.minY = minY;
+                this.width = width;
+                this.children = children;
+            }
+        }
+
+        public static List<Rect> GenerateColliders(BitBuffer solidBuffer)
+        {
+            Profiler.BeginSample("QuadTreeColliderGen.GenerateColliders");
+            List<Rect> rects = new List<Rect>();
+            TreeNode tree = CreateTree(solidBuffer, 0, 0, Grid2D.ChunkWidth);
+            RenderTree(tree, rects);
+            Profiler.EndSample();
+            return rects;
+        }
+
+        private static TreeNode CreateTree(BitBuffer solidTileBuffer, int minX, int minY, int width)
+        {
+            if (width == 1)
+            {
+                // Base case.
+                int index = Grid2D.GetChunkCellIndex(new Int2(minX, minY), Grid2D.ChunkWidth);
+                bool solid = solidTileBuffer.GetValue(index);
+                TreeNode.NodeValue value = solid ? TreeNode.NodeValue.Solid : TreeNode.NodeValue.Empty;
+                return new TreeNode(value, minX, minY, width, null);
+            }
+            else
+            {
+                // Recursive case.
+                int childWidth = width / 2;
+                TreeNode[] children = new TreeNode[4]
+                {
+                        CreateTree(solidTileBuffer, minX, minY, childWidth),
+                        CreateTree(solidTileBuffer, minX + childWidth, minY, childWidth),
+                        CreateTree(solidTileBuffer, minX, minY + childWidth, childWidth),
+                        CreateTree(solidTileBuffer, minX + childWidth, minY + childWidth, childWidth)
+                };
+
+                TreeNode.NodeValue value;
+                if (Array.TrueForAll(children, item => item.value == TreeNode.NodeValue.Solid))
+                {
+                    value = TreeNode.NodeValue.Solid;
+                }
+                else if (Array.TrueForAll(children, item => item.value == TreeNode.NodeValue.Empty))
+                {
+                    value = TreeNode.NodeValue.Empty;
+                }
+                else
+                {
+                    value = TreeNode.NodeValue.Partial;
+                }
+                return new TreeNode(value, minX, minY, width, children);
+            }
+        }
+
+        private static void RenderTree(TreeNode tree, List<Rect> results)
+        {
+            switch (tree.value)
+            {
+                case TreeNode.NodeValue.Solid:
+                    results.Add(new Rect(tree.minX, tree.minY, tree.width, tree.width));
+                    break;
+                case TreeNode.NodeValue.Partial:
+                    foreach (TreeNode node in tree.children) RenderTree(node, results);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
